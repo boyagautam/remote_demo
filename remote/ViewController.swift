20992b2a90c9ac491df2489ce9576f26baea6166
gautam <boyagautam@gmail.com>
2022-10-14 16:41:07 +0530

    Initial Commit

---
+++ remote/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  remote
+//
+//  Created by Goutham Boya on 14/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
